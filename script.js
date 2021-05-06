ec3c40d65e37417f13164aa761e53601fdfe46c7
Albert Pirverdyan <75203623+AlbertPirverdian@users.noreply.github.com>
2021-05-06 20:27:29 +0300

    первый комит

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+$.getJSON(" https://spreadsheets.google.com/feeds/list/18ybR6pmLiaJdFzc8nvGGqk6fzm0MBkqkUoh7YA8xpc8/od6/public/values?alt=json",
+function(data){
+    data = data['feed']['entry'] 
+    console.log(data);
+    show(data);
+}
+   )
+   function show(data){
+       var out = '';
+       for(var i = 0; i< data.length;i++){
+                    out+=`<div class="card">`;
+                    out+=`<h3 class="title"> ${data[i]['gsx$name']['$t']}</h3>`;
+                    out+=`<img src="${data[i]['gsx$image']['$t']}" alt="">`;
+                    out+=`<p class="price"> Цена:${data[i]['gsx$price']['$t']}</p>`; 
+                    out+=`</div>`;
+       }
+       document.querySelector('.shop_field').innerHTML = out;
+   }
+}
